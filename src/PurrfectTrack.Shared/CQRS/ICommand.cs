7729d57ac75725c877fe76179b7f5e51f58ce286
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-25 17:12:21 +0800

    feat: add command query responsibility segregation

---
+++ src/PurrfectTrack.Shared/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace PurrfectTrack.Shared.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+}
+
+public interface ICommand<out TResponse>
+    : IRequest<TResponse>
+{
+}
---
+++ src/PurrfectTrack.Shared/CQRS/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace PurrfectTrack.Shared.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IRequest<TResponse>
+    where TResponse : notnull
+{
+}
