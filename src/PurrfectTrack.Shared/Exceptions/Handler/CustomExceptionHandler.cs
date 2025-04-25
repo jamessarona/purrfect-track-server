dc62c5b672d0c7438341879b7ebeb0933ea02f08
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-25 17:13:07 +0800

    feat: add custom exception handler

---
+++ src/PurrfectTrack.Shared/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PurrfectTrack.Shared.Exceptions.Handler;
+
+public class CustomExceptionHandler
+    (ILogger<CustomExceptionHandler> logger)
+    : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError(
+            "Error Message: {exceptionMessage}, Time of occurence {time}",
+            exception.Message, DateTime.UtcNow);
+
+        (string Detail, string Title, int StatusCode) detail = exception switch
+        {
+            InternalServerException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            ),
+            ValidationException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            BadRequestException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            NotFoundException =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status404NotFound
+            ),
+            _ =>
+            (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            )
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = detail.Title,
+            Detail = detail.Detail,
+            Status = detail.StatusCode,
+            Instance = context.Request.Path
+        };
+
+        problemDetails.Extensions.Add("traiceId", context.TraceIdentifier);
+
+        if (exception is ValidationException validationException)
+            problemDetails.Extensions.Add("ValidationError", validationException.Errors);
+
+        await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+        return true;
+    }
+}
