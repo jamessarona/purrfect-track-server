bffd186c5a0ee3cc596ec7c9fdf6ff48af5564de
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-02 15:01:48 +0800

    feat: add custom domain exception

---
+++ src/PurrfectTrack.Domain/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿namespace PurrfectTrack.Domain;
+
+public class DomainException : Exception
+{
+    public string ErrorCode { get; }
+
+    public DomainException(string message, string errorCode = "DOMAIN_ERROR")
+        : base($"Domain Exception: \"{message}\" thrown from Domain Layer.")
+    {
+        ErrorCode = errorCode;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Error Code: {ErrorCode}";
+    }
+}
