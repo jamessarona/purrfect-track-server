dc16f6f267b10aa02320393678681d91ea59ce56
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-02 14:23:46 +0800

    refactor: change to nullable

--- src/PurrfectTrack.Domain/Entities/PetOwner.cs
+++ src/PurrfectTrack.Domain/Entities/PetOwner.cs
@@ -5,20 +5,38 @@ namespace PurrfectTrack.Domain.Entities;
 public class PetOwner : Entity<Guid>
 {
     [Required]
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = default!;
+
     [Required]
-    public string LastName { get; set; }
+    public string LastName { get; set; } = default!;
+    
     [Required]
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Address { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string Gender { get; set; }
+    public string Email { get; set; } = default!;
+
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Gender { get; set; }
 
     private readonly List<Pet> _pets = new();
     public IReadOnlyList<Pet> Pets => _pets.AsReadOnly();
 
-    public int NumberOfPets
+    public int NumberOfPets => _pets.Count;
+
+    protected PetOwner() { }
+
+    public PetOwner(string firstName, string lastName, string email, string? phoneNumber = null,
+        string? address = null, DateTime? dateOfBirth = null, string? gender = null)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        DateOfBirth = dateOfBirth;
+        Gender = gender;
+    }
     {
         get => Pets.Count;
         private set { }
