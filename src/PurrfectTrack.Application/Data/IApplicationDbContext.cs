71a4f03139a40cf11b09e7103ff4e62a59a5e702
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-03 16:11:49 +0800

    feat: setup database tables

---
+++ src/PurrfectTrack.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PurrfectTrack.Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<PetOwner> PetOwners { get; }
+    DbSet<Pet> Pets { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ src/PurrfectTrack.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Domain.Entities;
+using System.Reflection;
+
+namespace PurrfectTrack.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<PetOwner> PetOwners => Set<PetOwner>();
+    public DbSet<Pet> Pets => Set<Pet>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
