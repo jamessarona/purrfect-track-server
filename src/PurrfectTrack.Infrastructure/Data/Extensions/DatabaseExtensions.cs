956a2087699fd78c8881f4c08717217e9bf98aa4
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-03 16:12:34 +0800

    feat: implement data seeding (sample only)

---
+++ src/PurrfectTrack.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PurrfectTrack.Infrastructure.Data.Extensions;
+
+
+public static class DatabaseExtensions
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedAsync(context);
+    }
+
+    private static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedPetOwnerAsync(context);
+    }
+
+    private static async Task SeedPetOwnerAsync(ApplicationDbContext context)
+    {
+        if (!await context.PetOwners.AnyAsync())
+        {
+            //To be implemented
+            //await context.PetOwners.AddRangeAsync(InitialData.PetOwners);
+            //await context.SaveChangesAsync();
+        }
+    }
+}
