017f37e03ab366070b52703218c471ac14dab5fb
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-03 16:13:10 +0800

    feat: inject Application, Infrastructure, and API dependencies

---
+++ src/PurrfectTrack.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+using PurrfectTrack.Shared.Behaviors;
+using System.Reflection;
+
+namespace PurrfectTrack.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        services.AddFeatureManagement();
+
+        return services;
+    }
+}
---
+++ src/PurrfectTrack.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PurrfectTrack.Application.Abstractions;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Services;
+using PurrfectTrack.Infrastructure.Data;
+using PurrfectTrack.Infrastructure.Interceptors;
+using Microsoft.EntityFrameworkCore;
+
+namespace PurrfectTrack.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddScoped<IAuditService, AuditService>();
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetRequiredService<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
+}
