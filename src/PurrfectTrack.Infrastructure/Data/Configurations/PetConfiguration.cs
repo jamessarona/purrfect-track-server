2143b3d800b66d9e475df3f681807a769e6b7e09
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-03 16:12:04 +0800

    feat: implement pet and petowner configuration

---
+++ src/PurrfectTrack.Infrastructure/Data/Configurations/PetConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PurrfectTrack.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace PurrfectTrack.Infrastructure.Data.Configurations;
+
+public class PetConfiguration : IEntityTypeConfiguration<Pet>
+{
+    public void Configure(EntityTypeBuilder<Pet> builder)
+    {
+        builder.HasKey(o => o.Id);
+
+        builder.HasOne<Pet>()
+                .WithMany()
+                .HasForeignKey(oi => oi.PetOwnerId);
+        builder.Property(oi => oi.Name).HasMaxLength(50).IsRequired();
+    }
+}
---
+++ src/PurrfectTrack.Infrastructure/Data/Configurations/PetOwnerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PurrfectTrack.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace PurrfectTrack.Infrastructure.Data.Configurations;
+
+public class PetOwnerConfiguration : IEntityTypeConfiguration<PetOwner>
+{
+    public void Configure(EntityTypeBuilder<PetOwner> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.HasMany(o => o.Pets)
+            .WithOne()
+            .HasForeignKey(oi => oi.PetOwnerId);
+
+        builder.Property(p => p.FirstName).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.LastName).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.Email).HasMaxLength(50).IsRequired();
+    }
+}
