5345562bbf6be31ed5c55b5c58343fafa4840b71
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-03 16:14:20 +0800

    feat: add service and configure http request

--- src/PurrfectTrack.Api/Program.cs
+++ src/PurrfectTrack.Api/Program.cs
@@ -1,6 +1,29 @@
+using PurrfectTrack.Api;
+using PurrfectTrack.Application;
+using PurrfectTrack.Infrastructure;
+using PurrfectTrack.Infrastructure.Data.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. 
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
+
+
+// Run in Package Manager Console for Manual Migration Command
+// Add-Migration InitialCreate -OutputDir Data/Migrations -Project Ordering.Infrastructure -StartupProject Ordering.API
+// Update-Database
