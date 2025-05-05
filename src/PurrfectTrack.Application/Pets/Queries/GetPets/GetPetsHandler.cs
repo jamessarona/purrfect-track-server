62fa6b332f436b5484213faa260a13e01512d26c
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-05 12:38:58 +0800

    feat: add retrieval of pets

---
+++ src/PurrfectTrack.Application/Pets/Queries/GetPets/GetPetsHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Pets.Queries.GetPets;
+
+public class GetPetsHandler
+    : BaseQueryHandler, IQueryHandler<GetPetsQuery, GetPetsResult>
+{
+    public GetPetsHandler(IApplicationDbContext dbContext, IMapper mapper)
+        : base(dbContext, mapper) { }
+
+    public async Task<GetPetsResult> Handle(GetPetsQuery query, CancellationToken cancellationToken)
+    {
+        var pets = await dbContext.Pets
+            .ProjectTo<PetModel>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        return new GetPetsResult(pets);
+    }
+}
---
+++ src/PurrfectTrack.Application/Pets/Queries/GetPets/GetPetsQuery.cs
@@ -0,0 +1,9 @@
+﻿using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Pets.Queries.GetPets;
+
+public record GetPetsQuery()
+    : IQuery<GetPetsResult>;
+
+public record GetPetsResult(List<PetModel> Pets);
