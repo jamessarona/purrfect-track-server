67add50ae6e88bfddf7f2483f2d9e452f430f6a1
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-05 12:37:26 +0800

    feat: add create pet

---
+++ src/PurrfectTrack.Application/Pets/Commands/CreatePet/CreatePetCommand.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Pets.Commands.CreatePet;
+
+public record CreatePetCommand(Guid OwnerId, string Name, string? Species, string? Breed, string? Gender, DateTime? DateOfBirth, float? Weight, string? Color, bool IsNeutered)
+    : ICommand<CreatePetResult>;
+
+public record CreatePetResult(Guid Id);
+
+public class CreatePetCommandValidator : AbstractValidator<CreatePetCommand>
+{
+    public CreatePetCommandValidator()
+    {
+        RuleFor(x => x.OwnerId).NotNull().WithMessage("Owner is required");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    }
+}
---
+++ src/PurrfectTrack.Application/Pets/Commands/CreatePet/CreatePetHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+using PurrfectTrack.Shared.Exceptions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace PurrfectTrack.Application.Pets.Commands.CreatePet;
+
+public class CreatePetHandler
+    : BaseHandler, ICommandHandler<CreatePetCommand, CreatePetResult>
+{
+    public CreatePetHandler(IApplicationDbContext dbContext)
+        : base(dbContext) { }
+
+    public async Task<CreatePetResult> Handle(CreatePetCommand command, CancellationToken cancellationToken)
+    {
+        var owner = await dbContext.PetOwners
+            .Include(o => o.Pets)
+            .FirstOrDefaultAsync(o => o.Id == command.OwnerId, cancellationToken);
+
+        if (owner is null)
+            throw new OwnerNotFoundException(command.OwnerId);
+
+        owner.CreatePet(
+            command.OwnerId,
+            command.Name,
+            command.Breed,
+            command.Species,
+            command.Gender,
+            command.DateOfBirth,
+            command.Weight,
+            command.Color,
+            command.IsNeutered
+        );
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        var createdPet = owner.Pets.Last();
+        if (createdPet == null)
+            throw new Exception("Failed to create a pet.");
+
+        return new CreatePetResult(createdPet.Id);
+    }
+}
