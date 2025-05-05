8de3009a6120c3198141a44296e03ad8bfdfbd0e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-05 12:38:28 +0800

    feat: add custom exception for pet

---
+++ src/PurrfectTrack.Application/Exceptions/OwnerNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using PurrfectTrack.Shared.Exceptions;
+
+namespace PurrfectTrack.Application.Exceptions;
+
+class OwnerNotFoundException : NotFoundException
+{
+    public OwnerNotFoundException(Guid id) : base("Pet Owner", id)
+    {
+    }
+}
---
+++ src/PurrfectTrack.Application/Exceptions/PetNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using PurrfectTrack.Shared.Exceptions;
+
+namespace PurrfectTrack.Application.Exceptions;
+
+class PetNotFoundException : NotFoundException
+{
+    public PetNotFoundException(Guid id) : base("Pet", id)
+    {
+    }
+}
