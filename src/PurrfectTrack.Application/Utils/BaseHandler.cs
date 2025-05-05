e563e3eb97e0e8c68c00a1d17cdfae5aabce243b
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-05 12:37:10 +0800

    feat: add base handler for CQRS

---
+++ src/PurrfectTrack.Application/Utils/BaseHandler.cs
@@ -0,0 +1,13 @@
+﻿using PurrfectTrack.Application.Data;
+
+namespace PurrfectTrack.Application.Utils;
+
+public abstract class BaseHandler
+{
+    protected readonly IApplicationDbContext dbContext;
+
+    protected BaseHandler(IApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+}
---
+++ src/PurrfectTrack.Application/Utils/BaseQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PurrfectTrack.Application.Data;
+
+namespace PurrfectTrack.Application.Utils;
+public abstract class BaseQueryHandler : BaseHandler
+{
+    protected readonly IMapper mapper;
+
+    protected BaseQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        : base(dbContext)
+    {
+        this.mapper = mapper;
+    }
+}
