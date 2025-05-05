fb29ffbd6e34a0c5c8ac813c208b65952788206e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-05 12:39:07 +0800

    feat: add retrieval of pets by owner

---
+++ src/PurrfectTrack.Application/Pets/Queries/GetPetsByOwner/GetPetsByOwnerHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Pets.Queries.GetPetsByOwner;
+
+public class GetPetsByOwnerHandler 
+    : BaseQueryHandler, IQueryHandler<GetPetsByOwnerQuery, GetPetsByOwnerResult>
+{
+    public GetPetsByOwnerHandler(IApplicationDbContext dbContext, IMapper mapper)
+        : base(dbContext, mapper) { }
+
+    public async Task<GetPetsByOwnerResult> Handle(GetPetsByOwnerQuery query, CancellationToken cancellationToken)
+    {
+        var pets = await dbContext.Pets
+            .Where(p => p.OwnerId == query.OwnerId)
+            .ProjectTo<PetModel>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        return new GetPetsByOwnerResult(pets);
+    }
+}
---
+++ src/PurrfectTrack.Application/Pets/Queries/GetPetsByOwner/GetPetsByOwnerQuery.cs
@@ -0,0 +1,9 @@
+﻿using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Pets.Queries.GetPetsByOwner;
+
+public record GetPetsByOwnerQuery(Guid OwnerId) 
+    : IQuery<GetPetsByOwnerResult>;
+
+public record GetPetsByOwnerResult(List<PetModel> Pets);
