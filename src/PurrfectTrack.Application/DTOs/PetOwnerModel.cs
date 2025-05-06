2efd4b23abe957a8e0b3ce3cab37c126fac69f15
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-06 22:29:31 +0800

    feat: add pet owner dto

---
+++ src/PurrfectTrack.Application/DTOs/PetOwnerModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PurrfectTrack.Application.DTOs;
+
+public class PetOwnerModel
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Gender { get; set; }
+
+    public int NumberOfPets { get; set; }
+
+    public List<PetModel> Pets { get; set; } = new();
+}
