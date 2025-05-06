3d4f598bc57cb3d60bcb8094f86f1ab89b62dc6c
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-06 22:29:56 +0800

    feat: add create pet owner

---
+++ src/PurrfectTrack.Application/PetOwners/Commands/CreatePetOwner/CreatePetOwnerCommand.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using PurrfectTrack.Shared.CQRS;
+using System.ComponentModel.DataAnnotations;
+namespace PurrfectTrack.Application.PetOwners.Commands.CreatePetOwner;
+
+public record CreatePetOwnerCommand(string FirstName, string LastName, string Email, string? PhoneNumber, string? Address, DateTime? DateOfBirth, string? Gender)
+    : ICommand<CreatePetOwnerResult>;
+
+public record CreatePetOwnerResult(Guid Id);
+
+public class CreatePetOwnerCommandValidator : AbstractValidator<CreatePetOwnerCommand>
+{
+    public CreatePetOwnerCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("Firstname is required");
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Lastname is required");
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+    }
+}
---
+++ src/PurrfectTrack.Application/PetOwners/Commands/CreatePetOwner/CreatePetOwnerHandler.cs
@@ -0,0 +1,31 @@
+﻿using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Commands.CreatePetOwner;
+
+public class CreatePetOwnerHandler
+    : BaseHandler, ICommandHandler<CreatePetOwnerCommand, CreatePetOwnerResult>
+{
+    public CreatePetOwnerHandler(IApplicationDbContext dbContext)
+        : base(dbContext) { }
+
+    public async Task<CreatePetOwnerResult> Handle(CreatePetOwnerCommand command, CancellationToken cancellationToken)
+    {
+        var petOwner = new PetOwner(
+            command.FirstName,
+            command.LastName,
+            command.Email,
+            command.PhoneNumber,
+            command.Address,
+            command.DateOfBirth,
+            command.Gender);
+
+        dbContext.PetOwners.Add(petOwner);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+
+        return new CreatePetOwnerResult(petOwner.Id);
+    }
+}
