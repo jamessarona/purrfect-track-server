60a2726b3602d33cae12f484c7ff346ca961094e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-06 22:31:07 +0800

    feat: add pet owners query

---
+++ src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwners/GetPetOwnersHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Queries.GetPetOwners;
+
+public class GetPetOwnersHandler
+    : BaseQueryHandler, IQueryHandler<GetPetOwnersQuery, GetPetOwnersResult>
+{
+    public GetPetOwnersHandler(IApplicationDbContext dbContext, IMapper mapper) 
+        : base(dbContext, mapper) { }
+
+    public async Task<GetPetOwnersResult> Handle(GetPetOwnersQuery query, CancellationToken cancellationToken)
+    {
+        var petOwners = await dbContext.PetOwners
+            .Include(p => p.Pets)
+            .ProjectTo<PetOwnerModel>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        return new GetPetOwnersResult(petOwners);
+    }
+}
---
+++ src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwners/GetPetOwnersQuery.cs
@@ -0,0 +1,8 @@
+﻿using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Queries.GetPetOwners;
+
+public record GetPetOwnersQuery() : IQuery<GetPetOwnersResult>;
+
+public record GetPetOwnersResult(List<PetOwnerModel> PetOwners);
