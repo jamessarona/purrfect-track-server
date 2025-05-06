c650fb25a48d754dd9cafc61070bacabf95c27f0
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-06 22:30:33 +0800

    feat: add petowner query by id

---
+++ src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwnerById/GetPetOwnerByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Queries.GetPetOwnerById;
+
+public class GetPetOwnerByIdHandler
+    : BaseQueryHandler, IQueryHandler<GetPetOwnerByIdQuery, GetPetOwnerByIdResult>
+{
+    public GetPetOwnerByIdHandler(IApplicationDbContext dbContext, IMapper mapper) 
+        : base(dbContext, mapper) { }
+
+    public async Task<GetPetOwnerByIdResult> Handle(GetPetOwnerByIdQuery query, CancellationToken cancellationToken)
+    {
+        var petOwner = await dbContext.PetOwners
+            .Include(p => p.Pets)
+            .SingleOrDefaultAsync(p => p.Id == query.Id, cancellationToken);
+
+        if (petOwner is null)
+            throw new PetOwnerNotFoundException(query.Id);
+
+        var petOwnerModel = mapper.Map<PetOwnerModel>(petOwner);
+
+        return new GetPetOwnerByIdResult(petOwnerModel);
+    }
+}
---
+++ src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwnerById/GetPetOwnerByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Queries.GetPetOwnerById;
+
+public record GetPetOwnerByIdQuery(Guid Id) : IQuery<GetPetOwnerByIdResult>;
+
+public record GetPetOwnerByIdResult(PetOwnerModel Pet);
