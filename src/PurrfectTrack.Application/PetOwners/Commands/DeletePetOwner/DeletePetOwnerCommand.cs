42bd17187ab2e1cbdb0f9a51016122f911c7b401
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-06 22:30:19 +0800

    feat: add deletion of pet owner

---
+++ src/PurrfectTrack.Application/PetOwners/Commands/DeletePetOwner/DeletePetOwnerCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Commands.DeletePetOwner;
+
+public record DeletePetOwnerCommand(Guid Id)
+    : ICommand<DeletePetOwnerResult>;
+
+public record DeletePetOwnerResult(bool IsSuccess);
+
+public class DeletePetOwnerCommandValidator : AbstractValidator<DeletePetOwnerCommand>
+{
+    public DeletePetOwnerCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+    }
+}
---
+++ src/PurrfectTrack.Application/PetOwners/Commands/DeletePetOwner/DeletePetOwnerHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Commands.DeletePetOwner;
+
+public class DeletePetOwnerHandler
+    : BaseHandler, ICommandHandler<DeletePetOwnerCommand, DeletePetOwnerResult>
+{
+    public DeletePetOwnerHandler(IApplicationDbContext dbContext)
+        : base(dbContext) { }
+
+    public async Task<DeletePetOwnerResult> Handle(DeletePetOwnerCommand command, CancellationToken cancellationToken)
+    {
+        var petOwner = await dbContext.PetOwners
+            .FirstOrDefaultAsync(po => po.Id == command.Id, cancellationToken);
+
+        if (petOwner is null)
+            return new DeletePetOwnerResult(false);
+
+        dbContext.PetOwners.Remove(petOwner);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeletePetOwnerResult(true);
+    }
+}
