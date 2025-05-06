29ecfd4b5f233cfd1033e095186002cc67da61ee
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-06 22:30:45 +0800

    feat: add pet owner query by pet

---
+++ src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwnerByPet/GetPetOwnerByPetHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Queries.GetPetOwnerByPet;
+
+public class GetPetOwnerByPetHandler
+    : BaseQueryHandler, IQueryHandler<GetPetOwnerByPetQuery, GetPetOwnerByPetResult>
+{
+    public GetPetOwnerByPetHandler(IApplicationDbContext dbContext, IMapper mapper) 
+        : base(dbContext, mapper) { }
+
+    public async Task<GetPetOwnerByPetResult> Handle(GetPetOwnerByPetQuery query, CancellationToken cancellationToken)
+    {
+        var petOwner = await dbContext.PetOwners
+            .Where(po => po.Pets.Any(p => p.Id == query.PetId))
+            .Select(po => new
+            {
+                PetOwner = po,
+                Pets = po.Pets.Where(p => p.Id == query.PetId).ToList()
+            })
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (petOwner is null)
+            throw new PetOwnerNotFoundException(query.PetId);
+
+        var petOwnerModel = mapper.Map<PetOwnerModel>(petOwner.PetOwner);
+
+        petOwnerModel.Pets = mapper.Map<List<PetModel>>(petOwner.Pets);
+
+        return new GetPetOwnerByPetResult(petOwnerModel);
+    }
+}
---
+++ src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwnerByPet/GetPetOwnerByPetQuery.cs
@@ -0,0 +1,8 @@
+﻿using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Queries.GetPetOwnerByPet;
+
+public record GetPetOwnerByPetQuery(Guid PetId) : IQuery<GetPetOwnerByPetResult>;
+
+public record GetPetOwnerByPetResult(PetOwnerModel PetOwner);
