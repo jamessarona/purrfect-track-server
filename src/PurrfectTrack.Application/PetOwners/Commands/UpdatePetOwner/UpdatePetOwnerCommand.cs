4a20f6f9258544ce8257483f1b256a8d25990e04
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-06 22:30:09 +0800

    feat: add update pet owner

---
+++ src/PurrfectTrack.Application/PetOwners/Commands/UpdatePetOwner/UpdatePetOwnerCommand.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Commands.UpdatePetOwner;
+
+public record UpdatePetOwnerCommand(Guid Id, string FirstName, string LastName, string Email, string? PhoneNumber, string? Address, DateTime? DateOfBirth, string? Gender)
+    : ICommand<UpdatePetOwnerResult>;
+
+public record UpdatePetOwnerResult(Guid Id);
+
+public class UpdatePetOwnerCommandValidator : AbstractValidator<UpdatePetOwnerCommand>
+{
+    public UpdatePetOwnerCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("Firstname is required");
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Lastname is required");
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+    }
+}
---
+++ src/PurrfectTrack.Application/PetOwners/Commands/UpdatePetOwner/UpdatePetOwnerHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Commands.UpdatePetOwner;
+
+public class UpdatePetOwnerHandler
+    : BaseHandler, ICommandHandler<UpdatePetOwnerCommand, UpdatePetOwnerResult>
+{
+    public UpdatePetOwnerHandler(IApplicationDbContext dbContext) 
+        : base(dbContext) { }
+
+    public async Task<UpdatePetOwnerResult> Handle(UpdatePetOwnerCommand command, CancellationToken cancellationToken)
+    {
+        var petOwner = await dbContext.PetOwners
+            .FirstOrDefaultAsync(po => po.Id == command.Id, cancellationToken);
+
+        if (petOwner is null)
+            throw new PetOwnerNotFoundException(command.Id);
+
+        petOwner.FirstName = command.FirstName;
+        petOwner.LastName = command.LastName;
+        petOwner.Email = command.Email;
+        petOwner.PhoneNumber = command.PhoneNumber ?? petOwner.PhoneNumber;
+        petOwner.Address = command.Address ?? petOwner.Address;
+        petOwner.DateOfBirth = command.DateOfBirth ?? petOwner.DateOfBirth;
+        petOwner.Gender = command.Gender ?? petOwner.Gender;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdatePetOwnerResult(petOwner.Id);
+    }
+}
