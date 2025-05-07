8321f59fb19c865ad0a9bea1244ba96473d6be96
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-07 15:40:34 +0800

    feat: add users controller

---
+++ src/PurrfectTrack.Api/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PurrfectTrack.Application.PetOwners.Commands.DeletePetOwner;
+using PurrfectTrack.Application.Users.Commands.CreateUser;
+using PurrfectTrack.Application.Users.Commands.DeleteUser;
+using PurrfectTrack.Application.Users.Commands.UpdateUser;
+using PurrfectTrack.Application.Users.Queries.GetCurrentUser;
+using PurrfectTrack.Application.Users.Queries.GetUserById;
+using PurrfectTrack.Application.Users.Queries.GetUsers;
+using PurrfectTrack.Application.Users.Queries.GetUsersByRole;
+using PurrfectTrack.Shared.Pagination;
+using System.Threading;
+
+namespace PurrfectTrack.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mdiator)
+    {
+        _mediator = mdiator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return CreatedAtAction(nameof(GetUserById), new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UpdateUserCommand command, CancellationToken cancellationToken)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest("User ID mismatch");
+        }
+
+        var result = await _mediator.Send(command, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteUser(Guid id, CancellationToken cancellationToken)
+    {
+        var command = new DeleteUserCommand(id);
+        var result = await _mediator.Send(command, cancellationToken);
+        return result.IsSuccess ? NoContent() : NotFound();
+    }
+
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetUserById(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetUserByIdQuery(id);
+        var result = await _mediator.Send(query, cancellationToken);
+        return Ok(result.User);
+    }
+
+    [HttpGet("current")]
+    [Authorize]
+    public async Task<IActionResult> GetCurrentUser(CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetCurrentUserQuery(), cancellationToken);
+        return Ok(result.User);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers(CancellationToken cancellationToken, [FromQuery] int pageIndex = 0, [FromQuery] int pageSize = 20)
+    {
+        var query = new GetUsersQuery(new PaginationRequest(pageIndex, pageSize));
+        var result = await _mediator.Send(query, cancellationToken);
+        return Ok(result.Users);
+    }
+
+    [HttpGet("role")]
+    public async Task<IActionResult> GetUsersByRole(CancellationToken cancellationToken, [FromQuery] string role, [FromQuery] int pageIndex = 0, [FromQuery] int pageSize = 20)
+    {
+        var query = new GetUsersByRoleQuery(role, new PaginationRequest(pageIndex, pageSize));
+        var result = await _mediator.Send(query, cancellationToken);
+        return Ok(result.Users);
+    }
+}
