dbb2903839d123644cb67823629eeedae1448928
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-07 12:03:59 +0800

    feat: add custom exception for user

---
+++ src/PurrfectTrack.Application/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using PurrfectTrack.Shared.Exceptions;
+
+namespace PurrfectTrack.Application.Exceptions;
+
+class UserNotFoundException : NotFoundException
+{
+    public UserNotFoundException(Guid id) : base("User", id)
+    {
+    }
+}
