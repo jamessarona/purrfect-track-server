4ce9996325a7bf4f17b70577be5767103b1239eb
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-09 06:46:58 +0800

    feat: add uploading command of pet owner image

---
+++ src/PurrfectTrack.Application/PetOwners/Commands/UploadPetOwnerImage/UploadPetOwnerImageCommand.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.PetOwners.Commands.UploadPetOwnerImage;
+
+public record UploadPetOwnerImageCommand(Guid PetOwnerId, IFormFile File) 
+    : ICommand<UploadPetOwnerImageResult>;
+
+public record UploadPetOwnerImageResult(string ImageUrl);
+
+public class UploadPetOwnerImageValidator : AbstractValidator<UploadPetOwnerImageCommand>
+{
+    public UploadPetOwnerImageValidator()
+    {
+        RuleFor(x => x.PetOwnerId).NotEmpty().WithMessage("PetOwnerId is required");
+
+        RuleFor(x => x.File)
+            .NotNull().WithMessage("Image file is required")
+            .Must(file => file.Length > 0).WithMessage("Uploaded file is empty")
+            .Must(file => file.ContentType.StartsWith("image/")).WithMessage("Only image files are allowed");
+    }
+}
---
+++ src/PurrfectTrack.Application/PetOwners/Commands/UploadPetOwnerImage/UploadPetOwnerImageHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using PurrfectTrack.Application.Abstractions;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Infrastructure.Caching;
+using PurrfectTrack.Shared.CQRS;
+using PurrfectTrack.Shared.Exceptions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace PurrfectTrack.Application.PetOwners.Commands.UploadPetOwnerImage;
+
+public class UploadPetOwnerImageHandler
+    : BaseHandler, ICommandHandler<UploadPetOwnerImageCommand, UploadPetOwnerImageResult>
+{
+    private readonly ICacheService _cacheService;
+    private readonly IImageStorageService _imageService;
+
+    public UploadPetOwnerImageHandler(IApplicationDbContext dbContext, ICacheService cacheService, IImageStorageService imageService)
+        : base(dbContext)
+    {
+        _cacheService = cacheService;
+        _imageService = imageService;
+    }
+
+    public async Task<UploadPetOwnerImageResult> Handle(UploadPetOwnerImageCommand command, CancellationToken cancellationToken)
+    {
+        var petOwner = await dbContext.PetOwners
+            .Include(po => po.Pets)
+            .FirstOrDefaultAsync(po => po.Id == command.PetOwnerId, cancellationToken);
+
+        if (petOwner == null)
+            throw new PetOwnerNotFoundException(command.PetOwnerId);
+
+        var imageUrl = await _imageService.SaveImageAsync(command.File, "petowners", cancellationToken);
+        petOwner.ImageUrl = imageUrl;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        await _cacheService.RemoveAsync(CacheKeyManager.GetPetOwnersCacheKey());
+        await _cacheService.RemoveAsync(CacheKeyManager.GetPetOwnerByIdCacheKey(petOwner.Id));
+        foreach (var pet in petOwner.Pets)
+            await _cacheService.RemoveAsync(CacheKeyManager.GetPetOwnerByPetCacheKey(pet.Id));
+
+        return new UploadPetOwnerImageResult(imageUrl);
+    }
+}
