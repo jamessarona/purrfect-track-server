ce9d3adbbd94b92fcb5efebd8b5b81005665d0ca
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-09 00:54:55 +0800

    feat: add cachekey manager

---
+++ src/PurrfectTrack.Application/Utils/CacheKeyManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurrfectTrack.Infrastructure.Caching;
+
+public static class CacheKeyManager
+{
+    public static string GetPetOwnersCacheKey() => "petOwners";
+    public static string GetPetOwnerByIdCacheKey(Guid id) => $"petOwner-{id}";
+    public static string GetPetOwnerByPetCacheKey(Guid petId) => $"petOwnerByPet-{petId}";
+}
