499fc2a8338449ad0602bc765f61f51bea86dd23
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-10 03:30:21 +0800

    refactor: update petowner

--- src/PurrfectTrack.Domain/Entities/PetOwner.cs
+++ src/PurrfectTrack.Domain/Entities/PetOwner.cs
@@ -1,24 +1,7 @@
 ﻿namespace PurrfectTrack.Domain.Entities;
 
-public class PetOwner : Entity<Guid>
+public class PetOwner : Contact
 {
-    [Required]
-    public Guid UserId { get; set; }
-
-    public User User { get; set; } = null!;
-
-    [Required]
-    public string FirstName { get; set; } = default!;
-
-    [Required]
-    public string LastName { get; set; } = default!;
-
-    public string? PhoneNumber { get; set; }
-    public string? Address { get; set; }
-    public DateTime? DateOfBirth { get; set; }
-    public string? Gender { get; set; }
-    public string? ImageUrl { get; set; }
-
     public List<Pet> Pets { get; private set; } = new();
 
     public int NumberOfPets => Pets.Count;
@@ -28,14 +11,6 @@ public class PetOwner : Entity<Guid>
     public PetOwner(Guid userId, string firstName, string lastName,
         string? phoneNumber = null, string? address = null,
         DateTime? dateOfBirth = null, string? gender = null)
-    {
-        Id = Guid.NewGuid();
-        UserId = userId;
-        FirstName = firstName;
-        LastName = lastName;
-        PhoneNumber = phoneNumber;
-        Address = address;
-        DateOfBirth = dateOfBirth;
-        Gender = gender;
-    }
+        : base(userId, firstName, lastName, phoneNumber, address, dateOfBirth, gender)
+    { }
 }
