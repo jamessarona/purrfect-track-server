9c644aa5f9238f4e2472c01fc8796bc55685f73e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-10 02:27:37 +0800

    fix: remove redundant endpoint

--- src/PurrfectTrack.Api/Controllers/UsersController.cs
+++ src/PurrfectTrack.Api/Controllers/UsersController.cs
@@ -1,10 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using PurrfectTrack.Application.PetOwners.Commands.DeletePetOwner;
-using PurrfectTrack.Application.Users.Commands.CreateUser;
-using PurrfectTrack.Application.Users.Commands.DeleteUser;
-using PurrfectTrack.Application.Users.Commands.UpdateUser;
 using PurrfectTrack.Application.Users.Queries.GetCurrentUser;
 using PurrfectTrack.Application.Users.Queries.GetUserById;
 using PurrfectTrack.Application.Users.Queries.GetUsers;
@@ -24,36 +20,6 @@ public class UsersController : ControllerBase
     {
         _mediator = mdiator;
     }
-
-    [HttpPost]
-    [AllowAnonymous]
-    public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command, CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(command, cancellationToken);
-        return CreatedAtAction(nameof(GetUserById), new { id = result.Id }, result);
-    }
-
-    [HttpPut("{id:guid}")]
-    public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UpdateUserCommand command, CancellationToken cancellationToken)
-    {
-        if (id != command.Id)
-        {
-            return BadRequest("User ID mismatch");
-        }
-
-        var result = await _mediator.Send(command, cancellationToken);
-        return Ok(result);
-    }
-
-    [HttpDelete("{id:guid}")]
-    [Authorize(Roles = "Administrator")]
-    public async Task<IActionResult> DeleteUser(Guid id, CancellationToken cancellationToken)
-    {
-        var command = new DeleteUserCommand(id);
-        var result = await _mediator.Send(command, cancellationToken);
-        return result.IsSuccess ? NoContent() : NotFound();
-    }
-
 
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetUserById(Guid id, CancellationToken cancellationToken)
