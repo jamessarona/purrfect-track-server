6188609454e7f5f4b44521fca225531aa58ce204
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-10 03:29:47 +0800

    feat: add vet staff

---
+++ src/PurrfectTrack.Domain/Entities/VetStaff.cs
@@ -0,0 +1,22 @@
+﻿namespace PurrfectTrack.Domain.Entities;
+
+public class VetStaff : Contact
+{
+    public string? Position { get; set; }
+    public string? Department { get; set; }
+    public DateTime? EmploymentDate { get; set; }
+
+    protected VetStaff() { }
+
+    public VetStaff(Guid userId, string firstName, string lastName,
+        string? phoneNumber = null, string? address = null,
+        DateTime? dateOfBirth = null, string? gender = null,
+        string? position = null, string? department = null,
+        DateTime? employmentDate = null)
+        : base(userId, firstName, lastName, phoneNumber, address, dateOfBirth, gender)
+    {
+        Position = position;
+        Department = department;
+        EmploymentDate = employmentDate;
+    }
+}
