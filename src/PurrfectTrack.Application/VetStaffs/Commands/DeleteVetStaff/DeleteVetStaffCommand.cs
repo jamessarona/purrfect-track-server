b1590fe369a365a4be049de92830859337dce258
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-11 22:07:06 +0800

    feat: add delete vet staff command

---
+++ src/PurrfectTrack.Application/VetStaffs/Commands/DeleteVetStaff/DeleteVetStaffCommand.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.VetStaffs.Commands.DeleteVet;
+
+public record DeleteVetStaffCommand(Guid Id) : ICommand<DeleteVetStaffResult>;
+
+public record DeleteVetStaffResult(bool IsSuccess);
+
+public class DeleteVetStaffCommandValidator : AbstractValidator<DeleteVetStaffCommand>
+{
+    public DeleteVetStaffCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+    }
+}
---
+++ src/PurrfectTrack.Application/VetStaffs/Commands/DeleteVetStaff/DeleteVetStaffHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Infrastructure.Caching;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.VetStaffs.Commands.DeleteVet;
+
+public class DeleteVetStaffHandler
+    : BaseHandler, ICommandHandler<DeleteVetStaffCommand, DeleteVetStaffResult>
+{
+    private readonly ICacheService _cacheService;
+
+    public DeleteVetStaffHandler(IApplicationDbContext dbContext, ICacheService cacheService) 
+        : base(dbContext)
+    {
+        _cacheService = cacheService;
+    }
+
+    public async Task<DeleteVetStaffResult> Handle(DeleteVetStaffCommand command, CancellationToken cancellationToken)
+    {
+        var vetStaff = await dbContext.VetStaffs
+            .Include(v => v.User)
+            .FirstOrDefaultAsync(v => v.Id == command.Id, cancellationToken);
+
+        if (vetStaff is null)
+            return new DeleteVetStaffResult(false);
+        
+        await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+        try
+        {
+            dbContext.VetStaffs.Remove(vetStaff);
+            dbContext.Users.Remove(vetStaff.User);
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+        }
+        catch
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            throw;
+        }
+
+        await _cacheService.RemoveAsync(CacheKeyManager.GetVetStaffsCacheKey());
+        await _cacheService.RemoveAsync(CacheKeyManager.GetVetStaffByIdCacheKey(vetStaff.Id));
+
+        return new DeleteVetStaffResult(true);
+    }
+}
