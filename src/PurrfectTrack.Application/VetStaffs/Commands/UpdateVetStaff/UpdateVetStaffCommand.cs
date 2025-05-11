55b031469692d9dfda7826f4e622697f99f4cd9c
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-11 22:06:55 +0800

    feat: add update vet staff command

---
+++ src/PurrfectTrack.Application/VetStaffs/Commands/UpdateVetStaff/UpdateVetStaffCommand.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.VetStaffs.Commands.UpdateVet;
+
+public record UpdateVetStaffCommand(Guid Id, string FirstName, string LastName, string? PhoneNumber, string? Address, DateTime? DateOfBirth, string? Gender,
+        string? Position, string? Department, DateTime? EmploymentDate)
+    : ICommand<UpdateVetStaffResult>;
+
+public record UpdateVetStaffResult(Guid Id);
+
+public class UpdateVetStaffCommandValidator : AbstractValidator<UpdateVetStaffCommand>
+{
+    public UpdateVetStaffCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("Firstname is required");
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Lastname is required");
+    }
+}
---
+++ src/PurrfectTrack.Application/VetStaffs/Commands/UpdateVetStaff/UpdateVetStaffHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Infrastructure.Caching;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.VetStaffs.Commands.UpdateVet;
+
+public class UpdateVetStaffHandler
+    : BaseHandler, ICommandHandler<UpdateVetStaffCommand, UpdateVetStaffResult>
+{
+    private readonly ICacheService _cacheService;
+
+    public UpdateVetStaffHandler(IApplicationDbContext dbContext, ICacheService cacheService) 
+        : base(dbContext)
+    {
+        _cacheService = cacheService;
+    }
+
+    public async Task<UpdateVetStaffResult> Handle(UpdateVetStaffCommand command, CancellationToken cancellationToken)
+    {
+        var vetStaff = await dbContext.VetStaffs
+            .FirstOrDefaultAsync(v => v.Id == command.Id, cancellationToken);
+
+        if (vetStaff is null)
+            throw new VetStaffNotFoundException(command.Id);
+
+        vetStaff.FirstName = command.FirstName;
+        vetStaff.LastName = command.LastName;
+        vetStaff.PhoneNumber = command.PhoneNumber ?? vetStaff.PhoneNumber;
+        vetStaff.Address = command.Address ?? vetStaff.Address;
+        vetStaff.DateOfBirth = command.DateOfBirth ?? vetStaff.DateOfBirth;
+        vetStaff.Gender = command.Gender ?? vetStaff.Gender;
+        vetStaff.Position = command.Position ?? vetStaff.Position;
+        vetStaff.Department = command.Department ?? vetStaff.Department;
+        vetStaff.EmploymentDate = command.EmploymentDate ?? vetStaff.EmploymentDate;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        await _cacheService.RemoveAsync(CacheKeyManager.GetVetStaffsCacheKey());
+        await _cacheService.RemoveAsync(CacheKeyManager.GetVetStaffByIdCacheKey(vetStaff.Id));
+
+        return new UpdateVetStaffResult(vetStaff.Id);
+    }
+}
