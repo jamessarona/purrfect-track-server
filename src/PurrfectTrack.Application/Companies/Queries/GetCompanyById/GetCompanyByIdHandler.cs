69404eb1ab4205b63885092f2925e91fbb03c1f2
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-16 06:50:37 +0800

    feat: setup get company by id query

---
+++ src/PurrfectTrack.Application/Companies/Queries/GetCompanyById/GetCompanyByIdHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Companies.Queries.GetCompanyById;
+
+public class GetCompanyByIdHandler
+    : BaseQueryHandler, IQueryHandler<GetCompanyByIdQuery, GetCompanyByIdResult>
+{
+    public GetCompanyByIdHandler(IApplicationDbContext dbContext, IMapper mapper) 
+        : base(dbContext, mapper)
+    {
+    }
+
+    public async Task<GetCompanyByIdResult> Handle(GetCompanyByIdQuery query, CancellationToken cancellationToken)
+    {
+        var company = await dbContext.Companies
+            .FirstOrDefaultAsync(c => c.Id == query.Id, cancellationToken);
+
+        if (company is null)
+            throw new CompanyNotFoundException(query.Id);
+
+        var companyModel = mapper.Map<CompanyModel>(company);
+
+        return new GetCompanyByIdResult(companyModel);
+    }
+}
---
+++ src/PurrfectTrack.Application/Companies/Queries/GetCompanyById/GetCompanyByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Companies.Queries.GetCompanyById;
+
+public record GetCompanyByIdQuery(Guid Id) : IQuery<GetCompanyByIdResult>;
+
+public record GetCompanyByIdResult(CompanyModel Company);
