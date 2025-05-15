81dfa698bc7b540061da2617016d70f72069e699
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-16 06:50:50 +0800

    feat: expose company endpoint

---
+++ src/PurrfectTrack.Api/Controllers/CompanyController.cs
@@ -0,0 +1,75 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PurrfectTrack.Application.Companies.Commands.DeleteCompany;
+using PurrfectTrack.Application.Companies.Commands.UpdateCompany;
+using PurrfectTrack.Application.Companies.Queries.GetCompanies;
+using PurrfectTrack.Application.Companies.Queries.GetCompanyById;
+using PurrfectTrack.Application.Pets.Commands.CreatePet;
+using PurrfectTrack.Shared.Pagination;
+
+namespace PurrfectTrack.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompanyController : ControllerBase
+{
+    private readonly ISender _mediator;
+
+    public CompanyController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Administrator")]
+    public async Task<IActionResult> CreateCompany([FromBody] CreatePetCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return CreatedAtAction(nameof(GetCompanyById), new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id:guid}")]
+    [Authorize(Roles = "Administrator,Vet,VetStaff")]
+    public async Task<IActionResult> UpdateCompany(Guid id, [FromBody] UpdateCompanyCommand command, CancellationToken cancellationToken)
+    {
+        if (id != command.Id)
+            return BadRequest("Company ID mismatch");
+
+        var result = await _mediator.Send(command, cancellationToken);
+
+        if (result == null)
+            return NotFound();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [Authorize(Roles = "Administrator")]
+    public async Task<IActionResult> DeleteCompany(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new DeleteCompanyCommand(id), cancellationToken);
+
+            return result.IsSuccess ? NoContent() : NotFound();
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Administrator,PetOwner")]
+    public async Task<IActionResult> GetCompanies(CancellationToken cancellatioonToken, [FromQuery] int pageIndex = 0, [FromQuery] int pageSize = 20)
+    {
+        var query = new GetCompaniesQuery(new PaginationRequest(pageIndex, pageSize));
+
+        var result = await _mediator.Send(query, cancellatioonToken);
+
+        return Ok(result.Companies);
+    }
+
+    public async Task<IActionResult> GetCompanyById(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetCompanyByIdQuery(id);
+
+        var result = await _mediator.Send(query, cancellationToken);
+
+        return Ok(result.Company);
+    }
+}
---
+++ src/PurrfectTrack.Application/Exceptions/CompanyNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using PurrfectTrack.Shared.Exceptions;
+
+namespace PurrfectTrack.Application.Exceptions;
+
+class CompanyNotFoundException : NotFoundException
+{
+    public CompanyNotFoundException(Guid id) : base("Company", id)
+    { 
+    }
+}
