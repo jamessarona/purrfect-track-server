ba24651bca67f89e2aa999cbc23671ec4ef0b2d4
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-16 06:50:25 +0800

    feat: setup get companies query

---
+++ src/PurrfectTrack.Application/Companies/Queries/GetCompanies/GetCompaniesHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+using PurrfectTrack.Shared.Pagination;
+
+namespace PurrfectTrack.Application.Companies.Queries.GetCompanies;
+
+public class GetCompaniesHandler
+    : BaseQueryHandler, IQueryHandler<GetCompaniesQuery, GetCompaniesResult>
+{
+    public GetCompaniesHandler(IApplicationDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+    {
+    }
+
+    public async Task<GetCompaniesResult> Handle(GetCompaniesQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var companyQuery = dbContext.Companies.AsQueryable();
+
+        var totalCount = await companyQuery.CountAsync(cancellationToken);
+
+        var companies = await companyQuery
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        var companiesModels = mapper.Map<List<CompanyModel>>(companies);
+
+        var paginatedResult = new PaginatedResult<CompanyModel>(
+            pageIndex,
+            pageSize,
+            totalCount,
+            companiesModels
+        );
+
+        return new GetCompaniesResult(paginatedResult);
+    }
+}
---
+++ src/PurrfectTrack.Application/Companies/Queries/GetCompanies/GetCompaniesQuery.cs
@@ -0,0 +1,10 @@
+﻿using PurrfectTrack.Application.DTOs;
+using PurrfectTrack.Shared.CQRS;
+using PurrfectTrack.Shared.Pagination;
+
+namespace PurrfectTrack.Application.Companies.Queries.GetCompanies;
+
+public record GetCompaniesQuery(PaginationRequest PaginationRequest)
+    : IQuery<GetCompaniesResult>;
+
+public record GetCompaniesResult(PaginatedResult<CompanyModel> Companies);
