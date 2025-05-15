e0c737f2499f5257621828a200134f3d804fd247
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-16 06:49:57 +0800

    feat: add update company command

---
+++ src/PurrfectTrack.Application/Companies/Commands/UpdateCompany/UpdateCompanyCommand.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+using System.Windows.Input;
+
+namespace PurrfectTrack.Application.Companies.Commands.UpdateCompany;
+
+public record UpdateCompanyCommand(Guid Id, string Name, string? Description, string? PhoneNumber, string? Email, string? Website, string? Address, string TaxpayerId, bool IsActive)
+    : ICommand<UpdateCompanyResult>;
+
+public record UpdateCompanyResult(Guid Id);
+
+public class UpdateCompanyCommandValidator : AbstractValidator<UpdateCompanyCommand>
+{
+    public UpdateCompanyCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    }
+}
---
+++ src/PurrfectTrack.Application/Companies/Commands/UpdateCompany/UpdateCompanyHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Exceptions;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Companies.Commands.UpdateCompany;
+
+public class UpdateCompanyHandler
+    : BaseHandler, ICommandHandler<UpdateCompanyCommand, UpdateCompanyResult>
+{
+    public UpdateCompanyHandler(IApplicationDbContext dbContext) 
+        : base(dbContext)
+    {
+    }
+
+    public async Task<UpdateCompanyResult> Handle(UpdateCompanyCommand command, CancellationToken cancellationToken)
+    {
+        var company = await dbContext.Companies
+            .FirstOrDefaultAsync(c => c.Id == command.Id, cancellationToken);
+
+        if (company is null)
+            throw new CompanyNotFoundException(command.Id);
+
+        company.Name = command.Name;
+        company.Description = command.Description ?? company.Description;
+        company.PhoneNumber = command.PhoneNumber ?? company.PhoneNumber;
+        company.Email = command.Email ?? company.Email;
+        company.Website = command.Website ?? company.Website;
+        company.Address = command.Address ?? company.Address;
+        company.TaxpayerId = command.TaxpayerId ?? company.TaxpayerId;
+        company.IsActive = command.IsActive;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateCompanyResult(company.Id);
+    }
+}
