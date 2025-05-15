9d362e06ea65c082f13555132887f1251b537f39
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-15 16:42:34 +0800

    feat: add appointment dto

---
+++ src/PurrfectTrack.Application/DTOs/AppointmentModel.cs
@@ -0,0 +1,29 @@
+﻿using PurrfectTrack.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace PurrfectTrack.Application.DTOs;
+
+public class AppointmentModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string? Description { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? Location { get; set; }
+
+    public Guid PetOwnerId { get; set; }
+    public PetOwner? PetOwner { get; set; }
+
+    public Guid? PetId { get; set; }
+    public Pet? Pet { get; set; }
+
+    public Guid? VetId { get; set; }
+    public Vet? Vet { get; set; }
+
+    public Guid? VetStaffId { get; set; }
+    public VetStaff? VetStaff { get; set; }
+
+    public AppointmentStatus Status { get; set; }
+    public string? Notes { get; set; }
+}
