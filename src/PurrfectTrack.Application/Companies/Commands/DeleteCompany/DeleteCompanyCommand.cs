ab80f308cdeeda34a4bf479b386c4ced948f6ba2
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-16 06:50:10 +0800

    feat: setup delete company command

---
+++ src/PurrfectTrack.Application/Companies/Commands/DeleteCompany/DeleteCompanyCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Companies.Commands.DeleteCompany;
+
+public record DeleteCompanyCommand(Guid Id)
+    : ICommand<DeleteCompanyResult>;
+
+public record DeleteCompanyResult(bool IsSuccess);
+
+public class DeleteCompanyCommandValidator : AbstractValidator<DeleteCompanyCommand>
+{
+    public DeleteCompanyCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Name is required");
+    }
+}
---
+++ src/PurrfectTrack.Application/Companies/Commands/DeleteCompany/DeleteCompanyHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Companies.Commands.DeleteCompany;
+
+public class DeleteCompanyHandler
+    : BaseHandler, ICommandHandler<DeleteCompanyCommand, DeleteCompanyResult>
+{
+    public DeleteCompanyHandler(IApplicationDbContext dbContext) 
+        : base(dbContext)
+    {
+    }
+
+    public async Task<DeleteCompanyResult> Handle(DeleteCompanyCommand command, CancellationToken cancellationToken)
+    {
+        var company = await dbContext.Companies
+            .FirstOrDefaultAsync(c => c.Id == command.Id, cancellationToken);
+
+        if (company is null)
+            return new DeleteCompanyResult(false);
+
+        dbContext.Companies.Remove(company);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteCompanyResult(true);
+    }
+}
