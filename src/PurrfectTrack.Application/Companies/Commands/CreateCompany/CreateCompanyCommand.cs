9f9cfa633706fc581814edf5ab20f4f119dc3913
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-16 06:49:45 +0800

    feat: add create company command

---
+++ src/PurrfectTrack.Application/Companies/Commands/CreateCompany/CreateCompanyCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Companies.Commands.CreateCompany;
+
+public record CreateCompanyCommand(string Name, string? Description, string? PhoneNumber, string? Email, string? Website, string? Address, string TaxpayerId, bool IsActive)
+    : ICommand<CreateCompanyResult>;
+
+public record CreateCompanyResult(Guid Id);
+
+public class CreateCompanyCommandValidator: AbstractValidator<CreateCompanyCommand>
+{
+    public CreateCompanyCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    }
+}
---
+++ src/PurrfectTrack.Application/Companies/Commands/CreateCompany/CreateCompanyHandler.cs
@@ -0,0 +1,32 @@
+﻿using PurrfectTrack.Application.Data;
+using PurrfectTrack.Application.Utils;
+using PurrfectTrack.Shared.CQRS;
+
+namespace PurrfectTrack.Application.Companies.Commands.CreateCompany;
+
+public class CreateCompanyHandler
+    : BaseHandler, ICommandHandler<CreateCompanyCommand, CreateCompanyResult>
+{
+    public CreateCompanyHandler(IApplicationDbContext dbContext) 
+        : base(dbContext) { }
+
+    public async Task<CreateCompanyResult> Handle(CreateCompanyCommand command, CancellationToken cancellationToken)
+    {
+        var company = new Company(
+            command.Name,
+            command.Description,
+            command.PhoneNumber,
+            command.Email,
+            command.Website,
+            command.Address,
+            command.TaxpayerId,
+            command.IsActive
+           );
+
+        dbContext.Companies.Add(company);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateCompanyResult(company.Id);
+    }
+}
