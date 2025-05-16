698089d3f3dec2078ac86a13ea70fafc007d3684
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-16 21:09:47 +0800

    refactor: rename variable

--- src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwnerById/GetPetOwnerByIdQuery.cs
+++ src/PurrfectTrack.Application/PetOwners/Queries/GetPetOwnerById/GetPetOwnerByIdQuery.cs
@@ -5,4 +5,4 @@ namespace PurrfectTrack.Application.PetOwners.Queries.GetPetOwnerById;
 
 public record GetPetOwnerByIdQuery(Guid Id) : IQuery<GetPetOwnerByIdResult>;
 
-public record GetPetOwnerByIdResult(PetOwnerModel Pet);
+public record GetPetOwnerByIdResult(PetOwnerModel PetOwner);
